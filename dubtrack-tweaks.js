6fb312a588a899c73d359dd38e69f2c8ab9617b1
Patrick Nelson <patnelson@ebay.com>
2015-12-30 15:26:50 -0500

    Updated JS documentation to reference new/main README.md file on github.

--- dubtrack-tweaks.js
+++ dubtrack-tweaks.js
@@ -1,33 +1,17 @@
 /**
  * dubtrack-tweaks (originally "dubtrack-upvote")
  *
- * FEATURES:
- *
- *  - Automatically "updub" (upvote) songs a few seconds after they start (or visiting a room).
- *  - Allows you to mute music by simply pressing the spacebar anywhere within the window (except in chat).
- *  - Added the "Permalink" to the right of song titles so that now you can FINALLY visit *currently*
- *    playing SoundCloud (and YouTube) songs by simply clicking a link.
- *
- * INSTRUCTIONS:
- *
- * 1. In chrome, download/install Tampermonkey here: https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en
- * 2. In Tampermonkey options, go to "Create new Script"
- * 3. Copy/paste these entire contents.
- * 4. Visit dubtrack.fm and enter a room.
- * 5. Make sure Tampermonkey is enabled (click the Tampermonkey icon and select "Enabled").
- *
- * Note: These instructions are for Chrome users, but installation should be very similar if you're
- * using Firefox. For Firefox, you'll need to setup and use Greasemonkey instead of Tampermonkey
- * which can be found here: http://userscripts-mirror.org/about/installing.html
+ * For more information and installation instructions, please see:
+ * https://github.com/patricknelson/dubtrack-tweaks/
  */
 
 // ==UserScript==
 // @name         Dubtrack upvote
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Automatically "updub" (upvote) tracks after a few seconds.
 // @author       Patrick Nelson (pat@catchyour.com) a.k.a. chunk_split()
-// @site         https://gist.github.com/patricknelson/4b729ec234a35d4b8cc9
+// @site         https://github.com/patricknelson/dubtrack-tweaks/
 // @match        *://dubtrack.fm/*
 // @match        *://www.dubtrack.fm/*
 // ==/UserScript==
